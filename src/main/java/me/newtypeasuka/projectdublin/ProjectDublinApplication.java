71990b17a328b1b6a1534e5afda598418390ffa9
이상준 <sangzoon0102@naver.com>
2026-07-07 15:44:08 +0900

    첫 커밋

---
+++ src/main/java/me/newtypeasuka/projectdublin/ProjectDublinApplication.java
@@ -0,0 +1,15 @@
+package me.newtypeasuka.projectdublin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 스프링부트 시작점
+@SpringBootApplication
+public class ProjectDublinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDublinApplication.class, args);
+
+    }
+
+}
