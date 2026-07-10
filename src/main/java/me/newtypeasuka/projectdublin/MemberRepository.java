da1a29166251a6a011fc3b14e7b0838096f4910c
이상준 <sangzoon0102@naver.com>
2026-07-11 01:09:09 +0900

    h2 디비 향 테스트 코드

--- src/main/java/me/newtypeasuka/projectdublin/MemberRepository.java
+++ src/main/java/me/newtypeasuka/projectdublin/MemberRepository.java
@@ -1,8 +1,16 @@
 package me.newtypeasuka.projectdublin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByName(String name);
+
+    @Query("SELECT m FROM Member m WHERE m.name = ?1")
+    Optional<Member> findByNameQuery(String name);
 }
