34feb68c5bfebeb7226e26020e5526040c7d572c
이상준 <sangzoon0102@naver.com>
2026-07-16 20:17:14 +0900

    cicd.yml 오타 수정(3번째 시도)

--- src/main/java/me/newtypeasuka/projectdublin/controller/BlogViewController.java
+++ src/main/java/me/newtypeasuka/projectdublin/controller/BlogViewController.java
@@ -18,7 +18,6 @@ import java.util.List;
 public class BlogViewController {
 
     private final BlogService blogService;
-    
 
     @GetMapping("/articles")
     public String getArticles(Model model) {
