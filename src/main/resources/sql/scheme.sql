c1f4032a0ad5982764a8b809e4778c02c69e3333
이상준 <sangzoon0102@naver.com>
2026-07-22 02:10:40 +0900

    [디비] users 테이블 스키마 확정

--- src/main/resources/sql/scheme.sql
+++ src/main/resources/sql/scheme.sql
@@ -5,13 +5,17 @@ create database ProjectDublin
 use ProjectDublin;
 
 create table users (
-    id bigint not null auto_increment,
-    email varchar(255) not null,
-    nickname varchar(255),
+    id bigint unsigned not null auto_increment,
+    email varchar(320) not null,
+    nickname varchar(255) not null,
     password varchar(255),
-    created_at timestamp,
-    updated_at timestamp,
-    primary key (id)
+    role tinyint unsigned not null default 2 comment '1: admin / 2: user / 3: other',
+    auth_provider varchar(30) not null default 'GOOGLE',
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+    primary key (id),
+    constraint uq_users_email unique (email),
+    constraint ck_users_role check (role in (1, 2, 3))
 );
 
 create table articles (
