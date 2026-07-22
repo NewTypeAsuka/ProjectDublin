7875a6779edeb1fca2ef462abaf9b60ff24d2935
이상준 <sangzoon0102@naver.com>
2026-07-22 23:26:05 +0900

    [프론트]블로그 제목 클릭시 리다이렉트

--- src/main/resources/sql/scheme.sql
+++ src/main/resources/sql/scheme.sql
@@ -28,5 +28,26 @@ create table articles (
     primary key (id)
 );
 
+create table articles (
+    id bigint unsigned not null auto_increment,
+    author_id bigint unsigned not null,
+    title varchar(255) not null,
+    summary varchar(500) null,
+    content longtext not null,
+    view_count bigint unsigned not null default 0,
+    pinned tinyint(1) not null default 0,
+    language varchar(30) not null default 'korean',
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp,
+    primary key (id),
+    constraint ck_article_pinned check (pinned in (0, 1)),
+    constraint fk_article_author_id
+            foreign key (author_id) references users (id)
+            on update restrict  -- 이미 작성자가 있으면 작성자 수정 불가능
+            on delete restrict, -- 작성글이 하나라도 있으면 사용자는 삭제 불가능
+    index idx_article_author_id (author_id),
+    index idx_article_list_created (language, pinned, created_at, id)
+);
+
 select * from users;
 select * from articles;
